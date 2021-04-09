2e91983f02bcd463db245cd627616c4d4eb344bb
carlos-cypriano <81194143+carlos-cypriano@users.noreply.github.com>
2021-04-09 19:06:24 -0300

    IMPLEMETANDO MULTITHREADS EM CORRIDA DE GRILO
    Foi criado o método JumpingCricket() para aplicar o numero total de pulos e mecânica de deslocamento;
    Também foi criado o método printRacing() para imprimir a corrida;
    Foi criado o método placingCricket(0 para incrementar a colocação e imprimir a mesma;
    Foi criado o método run() que implementa a interface Runnable realizando um loop para que os métodos jumpingCricket() e printRacing() fossem chamados e ao final chamando o método de classificação placingCricket();
    Por fim na classe RacingCricket foram definidos a quantidade de grilos e a distancia total a ser percorrida.

--- CorridaDeGrilos/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá Carlos, aqui é sua classe principal, pode jogar o codigo aqui");
-    }
-}
