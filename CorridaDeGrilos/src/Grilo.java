6227ca2c44909ead7ad43761a0d7be3027765c7b
Rafael-720 <faelmknv@yahoo.com.br>
2021-04-10 04:07:43 -0300

    Update Grilo.java
    Foi criado o método Pular() atribuindo um valor randômico ao pulo do grilo, também foi criada uma variável para nomear o grilo

--- CorridaDeGrilos/src/Grilo.java
+++ CorridaDeGrilos/src/Grilo.java
@@ -1,3 +1,32 @@
+import java.util.Random;
+
 public class Grilo {
+
+    private int distanciaRandomica;
+    private String nome;
+    
+    public Grilo(String nome){
+        this.setNome(nome);
+    }
+    
+    public void pular(){
+        this.setDistanciaRandomica(new Random().nextInt(50));
+        //this.getDistanciaRandomica();
+    }
+    
+    public int getDistanciaRandomica() {
+        return distanciaRandomica;
+    }
+
+    public void setDistanciaRandomica(int distanciaRandomica) {
+        this.distanciaRandomica = distanciaRandomica;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
     
 }
