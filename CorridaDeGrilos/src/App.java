a107281faa5ec674d0670624a4142942f0a54fad
Rafael-720 <faelmknv@yahoo.com.br>
2021-04-10 04:44:00 -0300

    Update App.java
    Na classe principal é definida o numero de grilos e também o tamanho da corrida, e finalmente, iniciada a corrida.

--- CorridaDeGrilos/src/App.java
+++ CorridaDeGrilos/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+        //numero de grilos
+        int numGrilos = 4;
+        
+        //tamanho da corrida
+        int linhaChegada = 200;
+        
+        //instanciando grilos
+        Grilo[] grilos = new Grilo[numGrilos]; 
+        
+        //criando a corrida
+        Corrida corrida;
+        
+        //inicializando grilos
+        for(int i = 0; i < numGrilos; i++){
+            grilos[i] = new Grilo("Grilo_" + (i+1));
+        }
+        
+        
+        corrida = new Corrida(grilos, linhaChegada);
+        
     }
+    
 }
